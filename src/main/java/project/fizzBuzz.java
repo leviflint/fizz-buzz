3a1e3ac512c5ae253267d9e9e4f54767d1ffaea2
leviflint <tweekex@hotmail.com>
2020-09-15 13:50:18 -0400

    FizzBuzz interperet method finished

--- src/main/java/project/fizzBuzz.java
+++ src/main/java/project/fizzBuzz.java
@@ -4,6 +4,15 @@ public class fizzBuzz {
 
 
     public String interpret(int numberToSay) {
-        return "1";
+        if (numberToSay % 5 == 0 && numberToSay % 3 == 0){
+            return "fizzbuzz";
+        }
+        if (numberToSay % 3 == 0) {
+            return "fizz";
+        }
+        if (numberToSay % 5 == 0) {
+            return "buzz";
+        }
+        return "" + numberToSay;
     }
 }
--- src/test/java/project/fizzBuzzTest.java
+++ src/test/java/project/fizzBuzzTest.java
@@ -21,9 +21,45 @@ public class fizzBuzzTest {
         fizzBuzz undertest = new fizzBuzz();
 
         //Action
-        String result = undertest.interpret(1);
+        String result = undertest.interpret(4);
 
         //Assertion
-        assertEquals("1", result);
+        assertEquals("4", result);
+    }
+
+    @Test
+    public void returnFizz(){
+        //Arrange
+        fizzBuzz undertest = new fizzBuzz();
+
+        //Action
+        String result = undertest.interpret(3);
+
+        //Assertion
+        assertEquals("fizz", result);
+    }
+
+    @Test
+    public void returnBuzz(){
+        //Arrange
+        fizzBuzz undertest = new fizzBuzz();
+
+        //Action
+        String result = undertest.interpret(5);
+
+        //Assertion
+        assertEquals("buzz", result);
+    }
+
+    @Test
+    public void returnFizzBuzz(){
+        //Arrange
+        fizzBuzz undertest = new fizzBuzz();
+
+        //Action
+        String result = undertest.interpret(15);
+
+        //Assertion
+        assertEquals("fizzbuzz", result);
     }
 }
